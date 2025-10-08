d36d21cfbed98d7964d9b2b2a86496fd35211b67
BCA-Animesh <animeshdas.bca@gmail.com>
2025-10-08 21:42:04 +0530

    let's do

--- Blog-APP/src/Config.js
+++ Blog-APP/src/Config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref } from "firebase/database";
+import { getDatabase, ref, set, update } from "firebase/database";
 import Conf from './Conf'
 
 export class Survice{
@@ -9,7 +9,24 @@ export class Survice{
         this.app=initializeApp(Conf)
         this.database=getDatabase(this.app)
     }
-    
+    async createPost({userId, name, email, imageUrl}){
+        try {
+            await set(ref(this.database, `users/${userId}`),{
+                username:name,
+                email:email,
+                profile_picture:imageUrl
+            })
+        } catch (error) {
+            throw error   
+        }
+    }
+    async updatePost(userId, updatedPost){
+        try {
+            await update(ref(this.database, `users/${userId}`), updatedPost)
+        } catch (error) {
+            throw error
+        }
+    }
 }
 const survice= new Survice()
 
